244ab6aab41536cb9957c2446858080ba7983f09
ikramcase <ikramkhan483@gmail.com>
2023-10-07 16:22:35 +0100

    second commit

--- main.go
+++ main.go
@@ -1,15 +1,126 @@
 package main
 
 import (
+	"fmt"
 	"log"
+	"net/http"
 	"os"
 
 	"github.com/gofiber/fiber/v2"
 	"github.com/joho/godotenv"
+	"gorm.io/gorm"
 
 	"github.com/ikramcase/go-fibre/models"
 	"github.com/ikramcase/go-fibre/storage"
 )
+
+type User struct {
+	User     string `json:"user"`
+	Email    string `json:"email"`
+	Password string `json:"password"`
+}
+
+type Repository struct {
+	DB *gorm.DB
+}
+
+func (r *Repository) CreateUser(context *fiber.Ctx) error {
+	user := User{}
+
+	err := context.BodyParser(&user)
+
+	if err != nil {
+		context.Status(http.StatusUnprocessableEntity).JSON(
+			&fiber.Map{"message": "request failed"})
+		return err
+	}
+
+	err = r.DB.Create(&user).Error
+	if err != nil {
+		context.Status(http.StatusBadRequest).JSON(
+			&fiber.Map{"message": "could not create user"})
+		return err
+	}
+
+	context.Status(http.StatusOK).JSON(&fiber.Map{
+		"message": "user has been added"})
+	return nil
+}
+
+func (r *Repository) DeleteUser(context *fiber.Ctx) error {
+	userModel := models.User{}
+	id := context.Params("id")
+	if id == "" {
+		context.Status(http.StatusInternalServerError).JSON(&fiber.Map{
+			"message": "id cannot be empty",
+		})
+		return nil
+	}
+
+	err := r.DB.Delete(userModel, id)
+
+	if err.Error != nil {
+		context.Status(http.StatusBadRequest).JSON(&fiber.Map{
+			"message": "could not delete user",
+		})
+		return err.Error
+	}
+	context.Status(http.StatusOK).JSON(&fiber.Map{
+		"message": "user delete successfully",
+	})
+	return nil
+}
+
+func (r *Repository) GetUser(context *fiber.Ctx) error {
+	userModels := &[]models.User{}
+
+	err := r.DB.Find(userModels).Error
+	if err != nil {
+		context.Status(http.StatusBadRequest).JSON(
+			&fiber.Map{"message": "could not get users"})
+		return err
+	}
+
+	context.Status(http.StatusOK).JSON(&fiber.Map{
+		"message": "user fetched successfully",
+		"data":    userModels,
+	})
+	return nil
+}
+
+func (r *Repository) GetUserByID(context *fiber.Ctx) error {
+
+	id := context.Params("id")
+	userModel := &models.User{}
+	if id == "" {
+		context.Status(http.StatusInternalServerError).JSON(&fiber.Map{
+			"message": "id cannot be empty",
+		})
+		return nil
+	}
+
+	fmt.Println("the ID is", id)
+
+	err := r.DB.Where("id = ?", id).First(userModel).Error
+	if err != nil {
+		context.Status(http.StatusBadRequest).JSON(
+			&fiber.Map{"message": "could not get the user"})
+		return err
+	}
+	context.Status(http.StatusOK).JSON(&fiber.Map{
+		"message": "user id fetched successfully",
+		"data":    userModel,
+	})
+	return nil
+}
+
+func (r *Repository) SetupRoutes(app *fiber.App) {
+	api := app.Group("/api")
+	api.Post("/create_user", r.CreateUser)
+	api.Delete("delete_user/:id", r.DeleteUser)
+	api.Get("/get_user/:id", r.GetUserByID)
+	api.Get("/user", r.GetUser)
+}
 
 func main() {
 	// database
@@ -32,7 +143,7 @@ func main() {
 		log.Fatal("could not load the database")
 	}
 
-	err = models.MigrateBooks(db)
+	err = models.MigrateUser(db)
 	if err != nil {
 		log.Fatal("could not migrate db")
 	}
